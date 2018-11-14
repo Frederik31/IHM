36eedd118909d5d865c4f9d92cbdd76b750e4693
Jean-François Rolland <jean-francois.rolland@atos.net>
2018-11-14 12:22:09 +0100

    resolution de conflit

--- config/index.js
+++ config/index.js
@@ -46,9 +46,7 @@ module.exports = {
         pathRewrite: {
           '^/api': ''
         } */
-      }
-        changeOrigin: true
-      }
+        }
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
