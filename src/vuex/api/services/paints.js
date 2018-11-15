455433740395b297445b469d35423e82cf227328
Jean-François Rolland <jean-francois.rolland@atos.net>
2018-11-15 09:43:29 +0100

    ajout du boutton update sur le formulaire painture

--- src/vuex/api/services/paints.js
+++ src/vuex/api/services/paints.js
@@ -5,5 +5,13 @@ export default {
     return axios.get('/paints', request)
           .then((response) => Promise.resolve(response))
           .catch((error) => Promise.reject(error))
+  },
+
+  updatePaints (type, subtype, values) {
+    return axios.post('/paints', {
+      name: type, description: subtype, fields: values
+    })
+    .then((response) => Promise.resolve(response))
+    .catch((error) => Promise.reject(error))
   }
 }
